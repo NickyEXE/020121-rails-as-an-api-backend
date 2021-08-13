61633c8e942c363ee0e8c474707e25831039977f
NickyEXE <dovern42@gmail.com>
2021-08-13 12:18:36 -0400

    end of lecture

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -2,8 +2,14 @@ class CatsController < ApplicationController
 
 
   def index
-    @cats = Cat.all
-    render json: @cats
+    cats = Cat.all
+    render json: cats
+  end
+
+  def destroy
+    cat = Cat.find(params[:id])
+    cat.destroy
+    render json: {message: "You did it! You vanished a cat to the Thames."}
   end
 
 end
